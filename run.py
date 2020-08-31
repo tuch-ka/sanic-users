34235424b70ecad72fc553ff0c51b1dcb54c60ff
tuch-ka <tuch_ka@mail.ru>
2020-08-31 14:20:14 +0500

    init app

---
+++ run.py
@@ -0,0 +1,8 @@
+from src.app import create_app
+
+app = create_app()
+app.run(
+    host='0.0.0.0',
+    port=8001,
+    debug=True,
+)
---
+++ src/app.py
@@ -0,0 +1,7 @@
+from sanic import Sanic
+
+
+def create_app():
+    app = Sanic(__name__)
+
+    return app
