ada8013e59db2520a3ecc265fc980da6eebf373a
tuch-ka <tuch_ka@mail.ru>
2020-09-03 15:16:29 +0500

    add: users manage tests Только запросы без авторизации

---
+++ tests/test_user.py
@@ -0,0 +1,67 @@
+import asyncio
+import json
+
+import pytest
+
+from app import create_app
+
+
+@pytest.yield_fixture
+def app():
+    application = create_app()
+    yield application
+
+
+@pytest.fixture
+def test_cli(loop, app, sanic_client):
+    return loop.run_until_complete(sanic_client(app))
+
+
+#########
+# Tests #
+#########
+
+
+async def test_user_registry_no_password(test_cli):
+    """
+    POST /user/registry
+    """
+    user_data = {
+        'username': 'testuser',
+        'email': 'email@email.com',
+    }
+    response = await test_cli.post('/user/registry', data=json.dumps(user_data))
+    assert response.status == 400
+
+
+async def test_user_registry_no_username(test_cli):
+    """
+    POST /user/registry
+    """
+    user_data = {
+        'password': 'password',
+        'email': 'email@email.com',
+    }
+    response = await test_cli.post('/user/registry', data=json.dumps(user_data))
+    assert response.status == 400
+
+
+async def test_user_registry_good_data_with_flood(test_cli):
+    """
+    POST /user/registry
+    """
+    user_data = {
+        'username': 'testuser',
+        'password': 'password',
+        'email': 'email@email.com',
+        'foo': 'bar',
+    }
+
+    response = await test_cli.post('/user/registry', data=json.dumps(user_data))
+    assert response.status == 201 or response.status == 409
+
+    if response.status != 409:
+        response_json = await response.json()
+        assert user_data['username'] == response_json['username']
+        assert user_data['email'] == response_json['email']
+        assert 'foo' not in response_json
